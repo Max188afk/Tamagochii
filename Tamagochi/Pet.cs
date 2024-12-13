4f49dce6120c3ae6931c31d7581b4fae9d20c5ac
Max <syrvatkamax@gmail.com>
2024-12-13 09:03:49 +0100

    first commit

---
+++ Tamagochi/Pet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagochi
+{
+    public abstract class pet
+    {
+        //public int Hunger { get; private set; } = 50;
+        private int hunger = 50;
+        public int Hunger
+        {
+            get
+            { 
+                 return hunger;
+            }
+            set
+            {
+                hunger = value;
+            }
+         }
+        public int Boredom { get; private set; } = 50;
+        public void Feed()
+        {
+            Hunger -= 30;
+            Boredom += 15;
+            Console.WriteLine("Feeding");
+        }
+        public void Play()
+        {
+            Hunger += 30;
+            Boredom -= 15;
+            Console.WriteLine("Playing");
+        }
+        public bool isAlive()
+        {
+            if(Hunger>100 || Hunger<0)
+            {
+                return false;
+            }
+            return true;
+        }  
+             
+        public virtual string Whoami()
+        {
+            return "pet"; 
+        }
+    }
+}
---
+++ Tamagochi/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tamagochi;
+
+var cat = new Cat();
+var who = cat.Whoami();
+Console.WriteLine(who);
+cat.Feed();
+cat.Play ();
+cat.Play();
+cat.Play ();
+cat.Feed ();
+cat.Play ();
+cat.Feed();
+Console.WriteLine(cat.Hunger);
+Console.WriteLine(cat.Boredom);
+Console.WriteLine(cat.isAlive() ? "žije" : "nežije");
+
+
